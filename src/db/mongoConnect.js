4f33a9267fad4c1fc0ce63dbfa11771be241a62d
DiyorMarufov <marufovdiyor9@gmail.com>
2025-04-18 14:59:46 +0500

    envExample changde

--- src/db/mongoConnect.js
+++ src/db/mongoConnect.js
@@ -1,7 +1,7 @@
 import { connect } from "mongoose";
 
 export const mongoConnection = () => {
-  connect(process.env.URL)
+  connect(process.env.MONGO_URL)
     .then(() => console.log(`Mongo connected successfully`))
     .catch((err) => {
       console.error(`Mongo connection error:`, err);
