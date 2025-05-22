a9ea5be46aa17c30e28dc32b98e598bd4793adf6
DiyorMarufov <marufovdiyor9@gmail.com>
2025-05-22 18:45:02 +0500

    cleared

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CountriesModule } from './countries/countries.module';
-
-@Module({
-  imports: [CountriesModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
