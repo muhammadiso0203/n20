d04b1298e4778349c079397adfb75a472bea2889
DiyorMarufov <marufovdiyor9@gmail.com>
2025-06-16 20:27:05 +0500

    cleared

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = 4000;
-  await app.listen(PORT, () => console.log(`Server is running on port`, PORT));
-}
-bootstrap();
--- src/utils/error-catch.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException, InternalServerErrorException } from '@nestjs/common';
-
-export const errorCatch = (err: any) => {
-  if (err?.response) {
-    throw new HttpException(err?.response?.message, err?.response?.statusCode);
-  }
-  throw new InternalServerErrorException(err.message);
-};
